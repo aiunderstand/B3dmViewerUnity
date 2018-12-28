b6461bb2ea404cd1a159c9ec05f829b40f47eb71
Steven Bos <steven@stevenbos.com>
2018-12-28 18:51:26 +0100

    WIP Load amsterdam tileset (not implemented generic streaming, transform, materials, etc)

---
+++ Assets/TilesetSchema.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+namespace b3dm
+{
+    [Serializable]
+    public class Rootobject
+    {
+        public Asset asset { get; set; }
+        public int geometricError { get; set; }
+        public Root root { get; set; }
+    }
+
+    [Serializable]
+    public class Asset
+    {
+        public string version { get; set; }
+        public string gltfUpAxis { get; set; }
+    }
+
+    [Serializable]
+    public class Root
+    {
+        public Boundingvolume boundingVolume { get; set; }
+        public int geometricError { get; set; }
+        public Child[] children { get; set; }
+        public string refine { get; set; }
+        public float[] transform { get; set; }
+    }
+
+    [Serializable]
+    public class Boundingvolume
+    {
+        public float[] box { get; set; }
+    }
+
+    [Serializable]
+    public class Child
+    {
+        public Boundingvolume1 boundingVolume { get; set; }
+        public float geometricError { get; set; }
+        public Child[] children { get; set; }
+        public string refine { get; set; }
+        public Content content { get; set; }
+    }
+
+    [Serializable]
+    public class Boundingvolume1
+    {
+        public float[] box { get; set; }
+    }
+
+    [Serializable]
+    public class Content
+    {
+        public string url { get; set; }
+    }
+
+    [Serializable]
+    public class Child1
+    {
+        public Boundingvolume2 boundingVolume { get; set; }
+        public float geometricError { get; set; }
+        public Child2[] children { get; set; }
+        public string refine { get; set; }
+        public Content1 content { get; set; }
+    }
+
+    [Serializable]
+    public class Boundingvolume2
+    {
+        public float[] box { get; set; }
+    }
+
+
+    [Serializable]
+    public class Content1
+    {
+        public string url { get; set; }
+    }
+
+    [Serializable]
+    public class Child2
+    {
+        public Boundingvolume3 boundingVolume { get; set; }
+        public float geometricError { get; set; }
+        public Child3[] children { get; set; }
+        public string refine { get; set; }
+        public Content2 content { get; set; }
+    }
+
+    [Serializable]
+    public class Boundingvolume3
+    {
+        public float[] box { get; set; }
+    }
+
+    [Serializable]
+    public class Content2
+    {
+        public string url { get; set; }
+    }
+
+    [Serializable]
+    public class Child3
+    {
+        public Boundingvolume4 boundingVolume { get; set; }
+        public float geometricError { get; set; }
+        public Child4[] children { get; set; }
+        public string refine { get; set; }
+        public Content3 content { get; set; }
+    }
+
+    [Serializable]
+    public class Boundingvolume4
+    {
+        public float[] box { get; set; }
+    }
+
+    [Serializable]
+    public class Content3
+    {
+        public string url { get; set; }
+    }
+
+    [Serializable]
+    public class Child4
+    {
+        public Boundingvolume5 boundingVolume { get; set; }
+        public float geometricError { get; set; }
+        public Child5[] children { get; set; }
+        public string refine { get; set; }
+        public Content4 content { get; set; }
+    }
+
+    [Serializable]
+    public class Boundingvolume5
+    {
+        public float[] box { get; set; }
+    }
+
+    [Serializable]
+    public class Content4
+    {
+        public string url { get; set; }
+    }
+
+    [Serializable]
+    public class Child5
+    {
+        public Boundingvolume6 boundingVolume { get; set; }
+        public float geometricError { get; set; }
+        public Child6[] children { get; set; }
+        public string refine { get; set; }
+        public Content5 content { get; set; }
+    }
+
+    [Serializable]
+    public class Boundingvolume6
+    {
+        public float[] box { get; set; }
+    }
+
+    [Serializable]
+    public class Content5
+    {
+        public string url { get; set; }
+    }
+
+    [Serializable]
+    public class Child6
+    {
+        public Boundingvolume7 boundingVolume { get; set; }
+        public float geometricError { get; set; }
+        public Child7[] children { get; set; }
+        public string refine { get; set; }
+        public Content6 content { get; set; }
+    }
+
+    [Serializable]
+    public class Boundingvolume7
+    {
+        public float[] box { get; set; }
+    }
+
+    [Serializable]
+    public class Content6
+    {
+        public string url { get; set; }
+    }
+
+    [Serializable]
+    public class Child7
+    {
+        public Boundingvolume8 boundingVolume { get; set; }
+        public float geometricError { get; set; }
+        public object[] children { get; set; }
+        public string refine { get; set; }
+        public Content7 content { get; set; }
+    }
+
+    [Serializable]
+    public class Boundingvolume8
+    {
+        public float[] box { get; set; }
+    }
+
+    [Serializable]
+    public class Content7
+    {
+        public string url { get; set; }
+    }
+}
---
+++ Assets/b3dmStreamLoader.cs
@@ -0,0 +1,139 @@
+﻿using B3dm.Tile;
+using GLTF;
+using GLTF.Schema;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityGLTF;
+using Newtonsoft.Json;
+using b3dm;
+
+public class b3dmStreamLoader : MonoBehaviour
+{
+    //Notes on b3dm TRANSFORMS (see https://github.com/AnalyticalGraphicsInc/3d-tiles/tree/master/specification)
+
+    //Master transform    
+    //http://www.oc.nps.edu/oc2902w/coord/llhxyz.htm //convert ELEF coordinates to WGS84 lat, long, heigth;
+    //example: 3887868.815, 332860.248, 5028369.523 -> Lat 52.18818 deg N, Lon 4.89345 deg E, Height 6358446269.4 meter
+
+    //Child (of child of child ..) transform
+    //Bounding volume (box). The boundingVolume.box property is an array of 12 numbers that define an oriented bounding box in a right-handed 3-axis (x, y, z).
+    //Cartesian coordinate system where the z-axis is up.
+    //The first three elements define the x, y, and z values for the center of the box. 
+    //The next three elements (with indices 3, 4, and 5) define the x-axis direction and half-length.
+    //The next three elements(indices 6, 7, and 8) define the y-axis direction and half-length.
+    //The last three elements(indices 9, 10, and 11) define the z-axis direction and half-length.
+    //example root boundingbox 0: 0 1: 0 2: 0 3: 1740.124 4: 0 5: 0 6: 0 7: 2378.07 8: 0 9: 0 10: 0 11: 1256.995 
+    //0, 0, 0 Center
+    //1740.124, 0, 0 x as -> half length so Axis = 2* value
+    //0, 2378.07 y as -> half length so Axis = 2* value
+    //0,0, 1256.995 z as -> half length so Axis = 2* value
+
+    //example child[0] 0: 1.139 1: -167.195 2: 0 3: 1737.846 4: 0 5: 0 6: 0 7: 2043.679 8: 0 9: 0 10: 0 11: 1256.995 
+    //1.139, -167.195, 0 Center
+    //1737.846, 0, 0 x as -> half length so Axis = 2* value
+    //0, 2043.679 y as -> half length so Axis = 2* value
+    //0,0, 1256.995 z as -> half length so Axis = 2* value
+
+    private void Awake()
+    {
+        //get and parse tile.json 
+        string url = @"https://saturnus.geodan.nl/tomt/data/buildingtiles_adam/tileset.json";
+        StartCoroutine(DownloadTilesetJson(url));
+    }
+
+    private IEnumerator DownloadTilesetJson(string url)
+    {
+        DownloadHandlerBuffer handler = new DownloadHandlerBuffer();
+        var www = new UnityWebRequest(url)
+        {
+            downloadHandler = handler
+        };
+        yield return www.SendWebRequest();
+
+        if (!www.isNetworkError && !www.isHttpError)
+        {
+            //get data
+            string jsonString = www.downloadHandler.text;
+
+            //convert to jsonTree
+            var json = JsonConvert.DeserializeObject<b3dm.Rootobject>(jsonString);
+
+            //collect all url nodes in jsonTree and add to list
+            List<string> tiles = new List<string>();
+
+            foreach (var c in json.root.children)
+            {
+                tiles.Add(c.content.url);
+
+                if (c.children.Length > 0)
+                    AddToTiles(c.children, tiles);
+            }
+
+            //download and load tiles
+            StartCoroutine(ServerIsToSlowHack(tiles));
+        }
+        else
+        {
+            Debug.Log("Tile: [" + url + "] Error loading tileset data");
+        }
+    }
+
+    private IEnumerator ServerIsToSlowHack(List<string> tiles)
+    {
+        for (int i = 0; i < tiles.Count; i++)
+        {
+            StartCoroutine(DownloadB3DMTile(tiles[i]));
+            yield return null;
+        }
+    }
+
+    private void AddToTiles(Child[] children, List<string> tiles)
+    {
+        foreach (var c in children)
+        {
+            tiles.Add(c.content.url);
+
+            if (c.children.Length > 0)
+                AddToTiles(c.children, tiles);
+        }
+           
+    }
+
+    private IEnumerator DownloadB3DMTile(string url)
+    {
+        DownloadHandlerBuffer handler = new DownloadHandlerBuffer();
+        var www = new UnityWebRequest(@"https://saturnus.geodan.nl/tomt/data/buildingtiles_adam/" +url)
+        {
+            downloadHandler = handler
+        };
+        yield return www.SendWebRequest();
+
+        if (!www.isNetworkError && !www.isHttpError)
+        {
+            //get data
+            var stream = new MemoryStream(www.downloadHandler.data);
+
+            var b3dm = B3dmParser.ParseB3dm(stream);
+
+            var memoryStream = new MemoryStream(b3dm.GlbData);
+            Load(memoryStream);
+        }
+        else
+        {
+            Debug.Log("Tile: [" + url +"] Error loading b3dm data");
+        }
+    }
+
+    private void Load(Stream stream)
+    {
+        GLTFRoot gLTFRoot;
+        GLTFParser.ParseJson(stream, out gLTFRoot);
+        var loader = new GLTFSceneImporter(gLTFRoot, null, null, stream);
+        loader.LoadSceneAsync();
+    }
+
+}
