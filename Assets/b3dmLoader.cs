409d93d740a7962d33354c28a38da96944247433
Steven Bos <steven@stevenbos.com>
2018-12-28 15:53:46 +0100

    initial commit

---
+++ Assets/b3dmLoader.cs
@@ -0,0 +1,31 @@
+﻿using B3dm.Tile;
+using GLTF;
+using GLTF.Schema;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityGLTF;
+using UnityGLTF.Loader;
+
+public class b3dmLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        string path = Application.dataPath + "/1311.b3dm";
+        var fileStream = File.OpenRead(path);
+        var b3dm = B3dmParser.ParseB3dm(fileStream);
+
+        var memoryStream = new MemoryStream(b3dm.GlbData);
+        Load(memoryStream);
+    }
+
+    private void Load(Stream stream)
+    {
+            GLTFRoot gLTFRoot;
+            GLTFParser.ParseJson(stream, out gLTFRoot);
+            var loader = new GLTFSceneImporter(gLTFRoot, null, null, stream);
+            loader.LoadSceneAsync();
+    }
+}
